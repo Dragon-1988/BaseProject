d61dbf83b175a3a08a8ecfc1d079b191214d42d0
baydv <baydv@paditech.org>
2019-11-28 21:53:34 +0700

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by Michael Seven on 11/28/19.
+//  Copyright © 2019 Michael Seven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
