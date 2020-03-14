90ca7505309a3b77c18e20b65e2bd1af4e9d178d
baydv <baydv@paditech.org>
2020-03-14 10:34:12 +0700

    [Base] add base

---
+++ BaseProject/AppModule/SocialVM.swift
@@ -0,0 +1,9 @@
+//
+//  SocialVM.swift
+//  BaseProject
+//
+//  Created by Michael Seven on 2/16/20.
+//  Copyright © 2020 Michael Seven. All rights reserved.
+//
+
+import Foundation
---
+++ BaseProject/SplashVC.swift
@@ -0,0 +1,9 @@
+//
+//  SplashVC.swift
+//  BaseProject
+//
+//  Created by Michael Seven on 12/23/19.
+//  Copyright © 2019 Michael Seven. All rights reserved.
+//
+
+import Foundation
